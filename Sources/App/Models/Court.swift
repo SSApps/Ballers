17f542e329e21f53be91e6f1ca06c537ee749b01
ssapps <sspencer442@gmail.com>
2019-06-11 21:18:09 -0700

    Parent Court to Child User relationship added

--- Sources/App/Models/Court.swift
+++ Sources/App/Models/Court.swift
@@ -5,25 +5,52 @@ import FluentPostgreSQL
 final class Court: Codable {
     
     
-    var id: Int?
+    var id: UUID?
     var courtName: String
     var address: String
     var ballers: Int
-   // var users: [User.ID]?
     
     
-    init(courtName: String, address: String ){
+    
+    init(courtName: String, address: String, id: UUID ){
         
         self.courtName = courtName
         self.address = address
         self.ballers = 0
-       // self.users = users
+        
+        self.id  = id
         //
  
     }
 }
 
-extension Court: PostgreSQLModel {}
+extension Court{
+    var users: Children<Court, User>{
+        return children(\.courtID)
+    }
+}
+
+extension Court: PostgreSQLUUIDModel {}
 extension Court: Migration {}
 extension Court: Content {}
 extension Court: Parameter {}
+
+//struct AdminCourt: Migration {
+//    static func revert(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
+//         return .done(on: conn)
+//    }
+//    
+//    // 2
+//    typealias Database = PostgreSQLDatabase
+//    // 3
+//    static func prepare(on connection: PostgreSQLConnection)
+//        -> Future<Void> {
+//            // 4
+//            
+//            
+//            // 5
+//            let court = Court(courtName: "Mother", address: "123 Fake street", id: UUID())
+//            // 6
+//            return court.save(on: connection).transform(to: ())
+//    }
+//}
